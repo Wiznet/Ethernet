0e5fe09b3df8a9b112bc55c25810d1120c640f0c
taylor <taylor@wiznet.io>
2019-01-09 13:10:27 +0900

    Revert to Origin

--- examples/ChatServer/ChatServer.ino
+++ examples/ChatServer/ChatServer.ino
@@ -19,12 +19,13 @@
 #include <SPI.h>
 #include <Ethernet.h>
 
-//#define DEBUG_CHATSERVER_INO
+#define ON_TAYLOR_WORK_PC
+//#define DEBUG_CHATSERVER_INO_ETHERNET_BEGIN
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network.
 // gateway and subnet are optional:
-#if 1
+#if defined ON_TAYLOR_WORK_PC
 byte mac[] = {
 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192, 168, 0, 3);
@@ -54,6 +55,7 @@ void setup() {
   //Ethernet.init(15);  // ESP8266 with Adafruit Featherwing Ethernet
   //Ethernet.init(33);  // ESP32 with Adafruit Featherwing Ethernet
 
+#if defined DEBUG_CHATSERVER_INO_ETHERNET_BEGIN
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
    while (!Serial) {
@@ -62,6 +64,16 @@ void setup() {
 
   // initialize the ethernet device
   Ethernet.begin(mac, ip, myDns, gateway, subnet);
+#else
+  // initialize the ethernet device
+  Ethernet.begin(mac, ip, myDns, gateway, subnet);
+
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+#endif
 
   // Check for Ethernet hardware present
   if (Ethernet.hardwareStatus() == EthernetNoHardware) {
@@ -76,6 +88,7 @@ void setup() {
 
   // start listening for clients
   server.begin();
+
   Serial.print("Chat server address:");
   Serial.println(Ethernet.localIP());
 }
@@ -90,7 +103,7 @@ void loop() {
       // clear out the input buffer:
       client.flush();
       Serial.println("We have a new client");
-      //client.println("Hello, client!");
+      client.println("Hello, client!");
       alreadyConnected = true;
     }
 
@@ -98,14 +111,11 @@ void loop() {
       // read the bytes incoming from the client:
       char thisChar = client.read();
 
-      #if 0
-      PRINTVAR_HEX(thisChar);
-      #endif
-
       // echo the bytes back to the client:
       server.write(thisChar);
+
       // echo the bytes to the server as well:
-      //Serial.write(thisChar);   // Read Check
+      Serial.write(thisChar);   // Read Check
     }
   }
 }
